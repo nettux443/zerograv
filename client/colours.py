3d03a0ceb9ff73bf89c6f1c96d4fefe5643d0f38
lbourne43 <lewis.bourne@cognidox.com>
2014-04-23 13:32:43 +0100

    add colourize methods to colours module

--- client/colours.py
+++ client/colours.py
@@ -13,6 +13,24 @@ class alpha:
     YELLOW   = ( 255, 255,   0, 255)
     TRANSPARENT = (0,0,0,0)
     SEMI_TRANSPARENT = (0,0,0,128)
+    
+    def colourize(colour_string):
+        if colour == "yellow":
+            return  alpha.YELLOW
+        elif colour == "blue":
+            return  alpha.BLUE
+        elif colour == "cyan":
+            return  alpha.CYAN  
+        elif colour == "red":
+            return  alpha.RED
+        elif colour == "orange":
+            return  alpha.ORANGE
+        elif colour == "pink":
+            return  alpha.PINK
+        elif colour == "green":
+            return  alpha.GREEN
+        # the given colour couldn't be matched
+        return False
  
 class nonalpha:
     BLACK    = (   0,   0,   0)
@@ -27,4 +45,21 @@ class nonalpha:
     PINK     = ( 255,   0, 255)
     ORANGE   = ( 255, 165,   0)
     YELLOW   = ( 255, 255,   0)
+    def colourize(colour_string):
+        if colour == "yellow":
+            return  nonalpha.YELLOW
+        elif colour == "blue":
+            return  nonalpha.BLUE
+        elif colour == "cyan":
+            return  nonalpha.CYAN  
+        elif colour == "red":
+            return  nonalpha.RED
+        elif colour == "orange":
+            return  nonalpha.ORANGE
+        elif colour == "pink":
+            return  nonalpha.PINK
+        elif colour == "green":
+            return  nonalpha.GREEN
+        # the given colour couldn't be matched
+        return False
 
